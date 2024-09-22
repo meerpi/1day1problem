a9c7d8c1479e7d6208e479e333aa8b1ac891bfb7
meerpi <aritra.23bai10324@vitbhopal.ac.in>
2024-09-22 23:12:03 +0530

    Added new file

---
+++ day9/prob.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m,o,num,pp;
+    char s[100];
+    char *ptr = s;
+    scanf("%d",&n);
+    for(int i =0 ; i< 2*n;i++)
+    {
+        scanf("%d %d",&m , &o);
+        for(int j = 0; j < m; j++)
+        {
+            fgets(s , n ,stdin);
+            while(sscanf(ptr , "&d" , &num) == 1)
+            {
+                if (num > o)
+                {
+                    pp = num;
+                    num = 0
+                }
+            }
+
+        }
+    }
+
+}
