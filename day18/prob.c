1f341df61e4e8623a3a0af348dee02b2f222ceb4
meerpi <aritra.23bai10324@vitbhopal.ac.in>
2024-10-01 18:13:09 +0530

    Added new file

---
+++ day18/prob.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    char want[] = {'c','o','d','e','f','o','r','c','e','s'};
+    scanf("%d",&n);
+    int *res = (int*)malloc(sizeof(int) * n);
+    char word[11];
+    for(int i = 0; i<n; i++)
+    {   int count = 0;
+        scanf("%s",word);
+        for(int j = 0; j < 10 ; j++)
+        {
+            if(word[j] != want[j])
+            {
+                count ++;
+            }
+        }
+        res[i] = count;
+    }
+    for(int i = 0; i<n; i++)
+    {
+        printf("%d\n",res[i]);
+    }
+    return 0;
+}
--- readme.txt
+++ readme.txt
@@ -24,3 +24,4 @@ Welcome to my record of 1day1problem. I will be problem solving every day for at
 | Day 15 | 2024-09-28 |B. Borze | [https://codeforces.com/problemset/problem/32/B] (day015/) |
 | Day 16 | 2024-09-29 |A. Magnets | [https://codeforces.com/contest/344/problem/A] (day016/) |
 | Day 17 | 2024-09-30 |A. Square String | [https://codeforces.com/problemset/problem/1619/A] (day017/) |
+| Day 18 | 2024-09-30 |1829A - Love Story | [https://codeforces.com/problemset/problem/1829/A] (day018/) |
